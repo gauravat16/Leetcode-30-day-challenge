8af82d83e7abe17d1c9a485a5acc08afc2674e61
Gaurav Sharma <dev.gaurav94@gmail.com>
2020-04-18 14:31:32 +0530

    New code added

--- src/gaurav/leetcode/week3/MinimumPathSum.java
+++ src/gaurav/leetcode/week3/MinimumPathSum.java
@@ -3,26 +3,30 @@ package gaurav.leetcode.week3;
 public class MinimumPathSum {
 
     public int minPathSum(int[][] grid) {
-        int[][] minGrid = new int[grid.length][grid[0].length];
 
-        return minPathRecursive(grid, minGrid, 0, 0);
-
+        int[][] dp = new int[grid.length][grid[0].length];
+        return sum(grid, dp, 0, 0);
     }
 
-    public int minPathRecursive(int[][] grid, int[][] mem, int i, int j) {
 
-        if (i == grid.length - 1 && j == grid[0].length - 1) {
-            return grid[i][j];
-        }
-
-        if (i >= grid.length || j >= grid[0].length) {
+    private int sum(int[][] grid, int[][] dp, int r, int c){
+        if(r< 0 || c < 0 || r>=grid.length || c>=grid[0].length){
             return Integer.MAX_VALUE;
         }
 
-        if (mem[i][j] != 0) return mem[i][j];
+        if(dp[r][c] != 0){
+            return dp[r][c];
+        }
+
+        int down = sum(grid, dp, r+1, c);
+        int right =  sum(grid, dp, r, c+1);
+        int min = Math.min(down, right);
 
 
-        mem[i][j] = grid[i][j] + Math.min(minPathRecursive(grid, mem, i + 1, j), minPathRecursive(grid, mem, i, j + 1));
-        return mem[i][j];
+
+        dp[r][c] = (min != Integer.MAX_VALUE ? grid[r][c] + min : grid[r][c]);
+
+        return dp[r][c];
+
     }
 }
--- src/gaurav/leetcode/week3/NumberOfIslands.java
+++ src/gaurav/leetcode/week3/NumberOfIslands.java
@@ -4,34 +4,38 @@ public class NumberOfIslands {
 
     public int numIslands(char[][] grid) {
 
-        int count = 0;
+        if(grid == null || grid.length == 0){
+            return 0;
+        }
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
 
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == '1') {
-                    doDFS(grid, i, j);
+        int count = 0;
+        for(int r=0; r<grid.length; r++){
+            for(int c=0; c<grid[0].length; c++){
+                if(!visited[r][c] && grid[r][c] == '1'){
                     count++;
+                    markIsland(grid, r, c, visited);
                 }
             }
         }
 
         return count;
-
     }
 
-    void doDFS(char[][] grid, int x, int y) {
 
-        if (x >= grid.length || x < 0 || y >= grid[0].length || y < 0 || grid[x][y] != '1') {
+
+    void markIsland(char[][] grid, int r, int c, boolean[][] visited){
+        if(r< 0 || c <0 || r>= grid.length || c >= grid[0].length  || grid[r][c] == '0' || visited[r][c]){
             return;
         }
 
-        grid[x][y] = 'x';
 
-        doDFS(grid, x + 1, y);
-        doDFS(grid, x, y + 1);
-        doDFS(grid, x - 1, y);
-        doDFS(grid, x, y - 1);
+        visited[r][c] = true;
 
+        markIsland(grid, r+1, c, visited);
+        markIsland(grid, r-1, c, visited);
+        markIsland(grid, r, c+1, visited);
+        markIsland(grid, r, c-1, visited);
 
     }
 }
