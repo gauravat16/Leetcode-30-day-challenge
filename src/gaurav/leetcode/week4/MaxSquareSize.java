c0d8b372ed7b693fba1ddb2b8be3d8979507b8c9
Gaurav Sharma <dev.gaurav94@gmail.com>
2020-04-27 22:11:22 +0530

    Week 4 6th

---
+++ src/gaurav/leetcode/week4/MaxSquareSize.java
@@ -0,0 +1,29 @@
+package gaurav.leetcode.week4;
+
+public class MaxSquareSize {
+
+    class Solution {
+
+        public int maximalSquare(char[][] matrix) {
+
+            if (matrix.length == 0) {
+                return 0;
+            }
+
+            int[][] dp = new int[matrix.length + 1][matrix[0].length + 1];
+
+            int max = 0;
+            for (int r = 1; r < dp.length; r++) {
+                for (int c = 1; c < dp[r].length; c++) {
+                    if (matrix[r - 1][c - 1] == '1') {
+                        dp[r][c] = Math.min(dp[r][c - 1], Math.min(dp[r - 1][c], dp[r - 1][c - 1])) + 1;
+                        max = Math.max(max, dp[r][c]);
+                    }
+
+                }
+            }
+
+            return max * max;
+        }
+    }
+}
