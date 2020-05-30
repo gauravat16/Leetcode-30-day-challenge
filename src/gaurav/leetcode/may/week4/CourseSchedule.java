5a5afb3e37da8e4aafe2e30a0e12417a0d8aca3a
Gaurav Sharma <dev.gaurav94@gmail.com>
2020-05-30 12:37:47 +0530

    May_Week_4

---
+++ src/gaurav/leetcode/may/week4/CourseSchedule.java
@@ -0,0 +1,49 @@
+package gaurav.leetcode.may.week4;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class CourseSchedule {
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+        List<Integer>[] dependents = new LinkedList[numCourses];
+        int[] dependingOnArr = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            dependents[i] = new LinkedList<>();
+        }
+
+        for (int[] p : prerequisites) {
+            dependents[p[1]].add(p[0]);
+            dependingOnArr[p[0]]++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (dependingOnArr[i] == 0) {
+                q.offer(i);
+            }
+        }
+
+        int finished = 0;
+
+        while (!q.isEmpty()) {
+            int course = q.poll();
+            finished++;
+            for (int c : dependents[course]) {
+                dependingOnArr[c]--;
+                if (dependingOnArr[c] == 0) {
+                    q.offer(c);
+                }
+            }
+
+        }
+
+        return finished == numCourses;
+
+
+    }
+
+}
